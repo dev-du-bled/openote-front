e90a6283049bc54eeac68bfcd56bb82e09875fb9
Firmin B <stereogaming18@gmail.com>
2024-12-25 21:02:50 +0100

    style: Fixed favicon weird white square due to format

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,13 +4,15 @@ import vuetify, { transformAssetUrls } from "vite-plugin-vuetify";
 export default defineNuxtConfig({
   runtimeConfig: {
     public: {
-      api_base_url: process.env.OPENOTE_API_BASE_URL ?? "http://localhost:8000/api/v1",
+      api_base_url:
+        process.env.OPENOTE_API_BASE_URL ?? "http://localhost:8000/api/v1",
     },
   },
   app: {
     head: {
       titleTemplate: "%s - OpeNote",
       title: "Loading",
+      link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.png" }],
     },
   },
   compatibilityDate: "2024-04-03",
